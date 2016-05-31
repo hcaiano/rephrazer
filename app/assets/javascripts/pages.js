b495a831f511a2c26de109a406d5083c6094b18f
pacafs <paca.fs@gmail.com>
2016-05-31 15:40:24 +0100

    working translating

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,24 +1,71 @@
 $(function() {
+
+    $.FroalaEditor.DefineIcon('translate', {NAME: 'language'});
+      $.FroalaEditor.RegisterCommand('translate', {
+       title: 'Highlight as Code',
+       focus: false,
+       undo: false,
+       refreshAfterCallback: false,
+       callback: function (e){
+            var a = getSelectionText();
+            translate("en","pt", a.replace(/ /g,"%20"));  
+       }
+    });
+
+    function getSelectionText() {
+      var text = "";
+      if (window.getSelection) {
+          text = window.getSelection().toString();
+      } else if (document.selection && document.selection.type != "Control") {
+          text = document.selection.createRange().text;
+      }
+      return text;
+    }
+
+    function replaceSelectedText(replacementText) {
+      var sel, range;
+      if (window.getSelection) {
+          sel = window.getSelection();
+          if (sel.rangeCount) {
+              range = sel.getRangeAt(0);
+              range.deleteContents();
+              range.insertNode(document.createTextNode(replacementText));
+          }
+      } else if (document.selection && document.selection.createRange) {
+          range = document.selection.createRange();
+          range.text = replacementText;
+      }
+    }
+
+    function translate(source, target, q) {
+
+        $.ajax({
+          type: "GET",
+          url: "https://www.googleapis.com/language/translate/v2?key=AIzaSyC4MIHuhn_ZgedJ1iQPaTUwrMpx_Nngbd4" + "&source=" + source + "&target=" + target + "&q=" + q,
+          dataType: "json",
+          success: processData,
+        
+          error: function(){ 
+            alert("failed"); 
+          }
+
+        });
+
+        function processData(data)
+          {
+              var new_text = data.data.translations[0].translatedText;
+              replaceSelectedText(new_text);
+          }
+
+    }
+
     $('#edit').froalaEditor({
-      toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'color', 'paragraphStyle', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', 'undo', 'redo', 'clearFormatting', 'star'],
+      toolbarButtons: ['translate','fullscreen', 'bold', 'italic', 'underline', 'color', 'paragraphStyle', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', 'insertHR', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', 'undo', 'redo', 'clearFormatting', 'star'],
       toolbarSticky: true,
       toolbarInline: true,
       theme: 'gray',
       zIndex: 2003
     });
 
-    $('#edit').on('froalaEditor.toolbar.show', function (e, editor) {
-            var a = getSelectionText();
-            console.log(a.replace(/ /g,"%20"));
 
-            function getSelectionText() {
-                var text = "";
-                if (window.getSelection) {
-                    text = window.getSelection().toString();
-                } else if (document.selection && document.selection.type != "Control") {
-                    text = document.selection.createRange().text;
-                }
-                return text;
-            }
-    });
 });
